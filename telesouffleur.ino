2b95e8f1ff82de0304b6816712c7db9453ad3503
eywang <edmond.yizhou.wang@gmail.com>
2021-04-27 22:13:03 +0200

    add sentences

--- telesouffleur.ino
+++ telesouffleur.ino
@@ -6,34 +6,54 @@
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 //LiquidCrystal_I2C lcd(0x3F,20,4);
 
+String sentences[] = {
+  "Elle ment en Allemand !",
+  "Quand on n'a pas les moyens, on pic-nic.",
+  "Un face a face d'accord mais moi tout seul .",
+  "C'etait pas mauvais, c'etait tres mauvais !",
+  "Salomon... Slimane... Salomon... Slimane... vous seriez pas cousins ?",
+  "Philippe ! Je sais ou tu te caches ! Viens ici que je te bute encule !",
+  "Mon frere, je te vengerai ! Vous savez qui a tue mon frere ?",
+  "Je m'en fou des japonais et de leurs compliments, je veux sa tete !",
+  "L'impot sur le droit de vivre, ca ne vous dit rien ?",
+  "I beg your pardon my Lord. I am sure mais alors I am tout a fait sure",
+  "J'aime me beurrer la biscotte",
+  "Je connais cette theorie",
+  "Il y a forcement une amicale d'anciens nazis",
+  "Elle est comment votre blanquette ?",
+  "Vas-y !"
+};
+
+int sentencesCount = sizeof(sentences) / sizeof(sentences[0]);
+
 void setup()
 {
-  lcd.init(); // initialisation de l'afficheur
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 1);
+  lcd.print("  Bonjour, Maitre");
+  lcd.setCursor(0, 2);
+  lcd.print("  (K&R ed. fr p.6)");
+  delay(6000);
+  lcd.clear();
 }
 
 void loop()
 {
-  String sentences[] = {
-    "Test",
-    "Test 1",
-    "Test Sentence"
-  };
-
-  unsigned int sentencesCount = (unsigned int) sizeof(sentences) / sizeof(sentences[0]);
-
-  lcd.backlight();
+  randomSeed(millis() / 1000000);
+  int randomIndex = random(0, sentencesCount);
+  displaySentence(sentences[randomIndex]);
+  delay(8000);
   lcd.clear();
-  unsigned int r = random(0, sentencesCount);
-  displaySentence(sentences[r], SCREEN_COL);
-  delay(9500);
 }
 
-void displaySentence(String sentence, byte maxCol) {
-  byte currentLine = 0;
-  byte pos = 0;
+void displaySentence(String sentence) {
+  lcd.setCursor(0, 0);
+  int currentLine = 0;
+  int pos = 0;
 
-  for (byte index = 0; index < sentence.length(); index ++ ) {
-    if (pos >= maxCol) {
+  for (int index = 0; index < sentence.length(); index ++ ) {
+    if (pos >= SCREEN_COL) {
       pos = 0;
       currentLine++;
     }
